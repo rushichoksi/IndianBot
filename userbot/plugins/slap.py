bb9c1d9893d00c27adf5bd6b1edcf1ceb1ab3999
rushichoksi <67454775+rushichoksi@users.noreply.github.com>
2020-07-02 16:17:30 +0530

    Update slap.py

--- userbot/plugins/slap.py
+++ userbot/plugins/slap.py
@@ -24,6 +24,7 @@ SLAP_TEMPLATES = [
     "{user1} grabs up a {item} and {hits} {user2} with it.",
     "{user1} ties {user2} to a chair and {throws} a {item} at them.",
     "{user1} gave a friendly push to help {user2} learn to swim in lava."
+    "{user1} picked up a gun and fired a bullet to {user2}."
 ]
 
 ITEMS = [
